3543bec53cb617d4b1ae1a456efb5d2c56c8c6b3
peepo5 <72892531+peepo5@users.noreply.github.com>
2022-11-16 00:25:55 +0000

    Init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+import { Router, Route } from "svelte-navigator";
+import Home from "./routes/Home.svelte";
+
+</script>
+
+<Router>
+	<Route path="/">
+		<Home />
+	</Route>
+</Router>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,28 @@
+<script>
+let rome = false;
+
+</script>
+
+<main>
+	<div class="curs">
+	  	<img src="/xtc.png" width="300.1971px" alt="NR" />
+	</div>
+
+	<p>
+		<a href="https://ppmo.vercel.app">ppmo</a> | ...
+	</p>
+
+	<br><br><br><a on:click={() => rome = true} class="linc">... more ...</a>
+
+	{#if rome}
+	<div style="display: flex; justify-content: center;">
+	<p style="width:500px;">The collective is idea in time. An idea to regenerate the mind from poison using nonconventional techniques. To elevate one's selve above their current plane of existance -- past boundries and past themselves. This is freedom. More will come.</p>
+	</div>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+</style>
